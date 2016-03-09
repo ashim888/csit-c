0c86550d6e5901f135d528ce7e7b07e182867f87
Ashim Lamichhane <punchedrock@gmail.com>
2016-03-09 11:23:35 +0545

    added some codes

--- codes/pointer/memory3.c
+++ codes/pointer/memory3.c
@@ -4,13 +4,13 @@ int main()
 	/* code */
 	int v=10, *p;
 	p=&v;
-	printf("address of v=%u \n",&v );
+	printf("address of v=%p \n",&v );
 
-	printf("address of v=%u \n",p );
+	printf("address of v=%p \n",p );
 
 	printf("value of v=%d \n",v );
 	printf("value of v=%d \n",*p );
 
-	printf("address of p=%u \n",&p );
+	printf("address of p=%p \n",&p );
 	
 }
---
+++ codes/pointer/one_array_pointer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+	int x[5]={20,40,60,80,100},k;
+	printf("\narray element \t\telements value \t\taddress\n");
+	for(k=0;k<5;k++){
+		// printf("x[%d]\t\t\t%d\t\t\t%p\n",k,x[k],x+k );
+		printf("x[%d]\t\t\t%d\t\t\t%p\n",k,*(x+k),x+k );
+	}
+}
+
+
+
