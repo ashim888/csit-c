976a9a2e18fb6d0a44966f6505e384e40d9dd978
Ashim Lamichhane <punchedrock@gmail.com>
2016-03-14 11:04:52 +0545

    some code add

--- codes/pointer/one_array_pointer.c
+++ codes/pointer/one_array_pointer.c
@@ -5,7 +5,6 @@ int main(){
 	int x[5]={20,40,60,80,100},k;
 	printf("\narray element \t\telements value \t\taddress\n");
 	for(k=0;k<5;k++){
-		// printf("x[%d]\t\t\t%d\t\t\t%p\n",k,x[k],x+k );
 		printf("x[%d]\t\t\t%d\t\t\t%p\n",k,*(x+k),x+k );
 	}
 }
