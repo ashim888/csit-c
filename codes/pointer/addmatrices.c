5ee5f879aa2b826fb6ba5483391a027ac9782f3b
Ashim Lamichhane <punchedrock@gmail.com>
2016-03-20 10:52:50 +0545

    addmatrices issues fixed

--- codes/pointer/addmatrices.c
+++ codes/pointer/addmatrices.c
@@ -6,7 +6,7 @@
 
 int main(){
 	int (*a)[n]=malloc(sizeof(*a) * m);
-	int (*x)[n]=malloc(sizeof(*b) * m);
+	int (*b)[n]=malloc(sizeof(*b) * m);
 	int (*sum)[n]=malloc(sizeof(*sum) * m);;
 	// int a[m][n];
 	// int x[m][n];
@@ -22,14 +22,14 @@ int main(){
 	printf("Enter second matrix:\n");
 	for(i=0;i<m;i++){
 		for(j=0;j<n;j++){
-			scanf("%d",&x[i][j]);
+			scanf("%d",&b[i][j]);
 		}
 	}	
 
 	printf("The Sum of matrix is:\n");
 	for(i=0;i<m;i++){
 		for(j=0;j<n;j++){
-			*(*(sum+i)+j)=*(*(a+i)+j)+*(*(x+i)+j);
+			*(*(sum+i)+j)=*(*(a+i)+j)+*(*(b+i)+j);
 			printf("\t%d",*(*(sum+i)+j));
 		}
 		printf("\n");
