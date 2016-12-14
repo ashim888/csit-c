19357a59f28749d0c279b31d868b82c460bef21b
ashim888 <punchedrock@gmail.com>
2016-12-14 21:42:50 +0545

    will be uploaded later

--- codes/pointer/nullpoint.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-	int *p=NULL;
-	int a,b;
-	printf("%p\n",&p);
-	printf("%d\n",a/b);
-}
--- codes/pointer/search.c
+++
@@ -1,4 +0,0 @@
-/* Write a function that uses pointers to search for the address of a given integer in a given array.
-If the given integer is found, the function returns its address; otherwise it returns NULL. */
-
-#include <stdio.h>
