450d96194b018df3258a2435a126cc3ee3b0e8b4
ashim888 <punchedrock@gmail.com>
2017-01-11 00:47:39 +0545

    assignment 3

---
+++ Assignments/assignment_3/README.md
@@ -0,0 +1,18 @@
+###ASSIGNMENT III
+
+DEADLINE 16th January,2017
+---------------------------
+
+1.	WAP to display Sum of two numbers
+2.	WAP to display if the number is positive or negative
+3.	WAP to display whether the number is even or odd.
+4.	WAP to read three numbers and print the greatest number.
+5.	WAP to find the sum of the series 1+2+3+4….. Up to entered n numbers.
+6.	WAP to display factorial of a given number N
+7.	WAP to read first 20 number and display only sum of even numbers.
+8.	WAP to read a number n and display all of its divisors.
+9.	WAP which displays if a number is prime or not.
+10. What do you mean by Precedence And Associativity Of Operators in C. Explain with some examples.
+
+
+Note: At this stage in your education you are responsible for your own work; I will not accept any excuses for plagiarism regardless of your intention.
--- codes/basics/test.c
+++ codes/basics/test.c
@@ -4,7 +4,7 @@
 int main(){
 	int value_test=1;
 	//int value_test=4294967295;
-	printf("the value of a is %u",value_test);
+	printf("the value of a is %u\n",value_test);
 	//printf("size of an integer value_test is %lu",sizeof(value_test));	
 
 	/*
@@ -16,4 +16,31 @@ int main(){
 	ashim nepal=56;
 	printf("value of ashim nepal is %d",nepal);
 	*/
+
+	int a=60,b=15;
+	// printf("%d\n", a-b);
+	// printf("%d\n", b-a);
+	// printf("%d\n", b%a);
+	// printf("%d\n", a%b);
+
+	// printf("a =%d\n",a);
+	// printf("a =%d\n",++a);
+	// printf("a =%d\n",a++);
+	// printf("a =%d\n",a);
+
+	// Conditional operator
+	// int larger= a>b ? a : b;
+	// printf("The larger number is %d\n", larger);
+
+	// Bitwise And
+	// printf("%lu\n",sizeof(a));
+	// printf("%d\n",a&b);	
+
+	// Bitwise Shift
+	printf("%d\n",a<<3 );
+	printf("%d\n",a>>4 );
+	printf("%d\n",~a );
+
+	int c=(a=1,b=2,a+b);
+	printf("%d\n",c );	
 }
