db58fe144af913ce31460247ccf7d04c36141cc5
ashim88 <punchedrock@gmail.com>
2017-01-19 01:03:58 +0545

    prime fix

--- codes/basics/primeornot.c
+++ codes/basics/primeornot.c
@@ -1,34 +1,20 @@
-// THIS PROGRAM WAS FROM STUDENT WHICH DOESN'T WORK FOR NUMBERS
 #include <stdio.h>
-
 int main(){
-	int c=0,n,i=2;
-	for (int j=1; j < 100; j++)
-	{
-		// printf("Enter a vnumber: ");
-		// scanf("%d",&num);
-		n=j/2;
-
-		for(i=2;i<n;i++){
-			if (j % i == 0)
-			{
-				c=0;
-				break;
-			}
-			else{
-				c=1;
-			}
-		}
-		// if(c==0){
-		// 	printf("%d is not a prime number\n",j );
-		// }else{
-		// 	printf("%d is a prime number\n",j );
-		// }
-		if (c!=0)
+	int max_limit,flag=0;
+	printf("Enter any number: ");
+	scanf("%d",&max_limit);
+	for (int i = 1; i <= max_limit; i++)
+	{	
+		flag=0;
+		for (int j = 1; j<=i; j++)
 		{
-			printf("%d is a prime number\n",j );
+			if (i%j==0){
+				flag++;
+			}
 		}
+		if (flag==2){
+			printf("%d is prime\n",i );
+		}	
 	}
-	// 
-
+	return 0;
 }
