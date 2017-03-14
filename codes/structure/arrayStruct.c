118f1bba5d8f4b761b8f7086ef20840212658626
ashim888 <punchedrock@gmail.com>
2017-03-14 23:22:23 +0545

    change of size

--- codes/structure/arrayStruct.c
+++ codes/structure/arrayStruct.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define m 2
 
 struct student{
     char name[50];
@@ -7,10 +8,10 @@ struct student{
 };
 
 int main(){
-    struct student s[10];
+    struct student s[m];
     int i;
     printf("Enter information of students:\n");
-    for(i=0;i<10;++i)
+    for(i=0;i<m;++i)
     {
         s[i].roll=i+1;
         printf("\nFor roll number %d\n",s[i].roll);
@@ -21,12 +22,12 @@ int main(){
         printf("\n");
     }
     printf("Displaying information of students:\n\n");
-    for(i=0;i<10;++i)
+    for(i=0;i<m;++i)
     {
-     printf("\nInformation for roll number %d:\n",i+1);
-     printf("Name: ");
-     puts(s[i].name);
-     printf("Marks: %.1f",s[i].marks);
+        printf("\nInformation for roll number %d:\n",i+1);
+        printf("Name: %s",s[i].name);
+        // puts(s[i].name);
+        printf("Marks: %.1f",s[i].marks);
    }
    return 0;
 }
