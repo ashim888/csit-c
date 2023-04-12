bd26bbfae2c1f815e6736dfce1f8d9f07f55eeae
basab  lol <123973908+chillobae@users.noreply.github.com>
2023-04-12 08:14:43 +0545

    Update palindromeString.c

--- codes/arrays/palindromeString.c
+++ codes/arrays/palindromeString.c
@@ -4,29 +4,40 @@ reference:
 http://www.w3schools.in/c/program/check-palindrome-string/
 */
 
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
+#include<string.h>
 
-int main(void){
+int main(void) {
     char string1[20];
-    int i, length;
-    int flag = 0;
-    
+    int i, length, flag = 0;
+
     printf("Enter a string:");
     scanf("%s", string1);
-    
-    for(i=0;i < strlen(string1);i++){
-        if(string1[i] != string1[length-i-1]){
+
+    length = strlen(string1);
+
+    for(i = 0; i < length; i++) {
+        if(string1[i] != string1[length - i - 1]) {
             flag = 1;
             break;
-	   }
-	}
-    
-    if (flag) {
+        }
+    }
+
+    if(flag) {
         printf("%s is not a palindrome\n", string1);
     }    
     else {
         printf("%s is a palindrome\n", string1);
     }
-    
+
+    return 0;
 }
+
+
+/* edits made:
+1.initialized flag to 0 in the same line it was declared.
+2.added a statement to calculate the length of the string using strlen.
+3.changed the loop condition to i < length.
+4.changed the comparison in the loop to string1[length - i - 1].
+5.added a return 0 statement at the end of main.
+*/
